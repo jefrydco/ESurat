5d768fba332872c84938104361c2846cfcdeb64a
Margareta Widya Agustina <margaretawdya@gmail.com>
2018-07-26 10:56:06 +0700

    Add HomeConstant and HomeService

---
+++ app/src/main/java/com/example/esurat/home/HomeConstant.java
@@ -0,0 +1,16 @@
+package com.example.esurat.home;
+
+public class HomeConstant {
+    public static final String ID = "id";
+    public static final String RANK = "rank";
+    public static final String PERIHAL = "perihal";
+    public static final String DARI = "dari";
+    public static final String TANGGAL_TERIMA = "tanggalTerima";
+    public static final String STATUS = "status";
+    public static final String NO_AGENDA = "noAgenda";
+    public static final String NO_SURAT = "noSurat";
+    public static final String SIFAT = "sifat";
+    public static final String TANGGAL_SURAT = "tanggalSurat";
+    public static final String KETERANGAN = "keterangan";
+    public static final String LINK_LIHAT_SURAT = "linkLihatSurat";
+}
---
+++ app/src/main/java/com/example/esurat/home/HomeService.java
@@ -0,0 +1,10 @@
+package com.example.esurat.home;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public class HomeService {
+
+/*    @GET("input-api-list-disini")
+    Call<String> getHomeList();*/
+}
